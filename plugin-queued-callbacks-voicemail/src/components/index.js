2233b20e82911d72e6466c8a900d6a94a14f6b3f
trogers-twilio <46937354+trogers-twilio@users.noreply.github.com>
2022-09-20 16:21:21 -0700

    Cleaned up code and invalid references

--- plugin-queued-callbacks-voicemail/src/components/index.js
+++ plugin-queued-callbacks-voicemail/src/components/index.js
@@ -1,2 +1,1 @@
-export { default as CallbackComponent } from './callback';
-export { default as VoicemailComponent } from './voicemail';
+export { default as VoicemailInfoTab } from './VoicemailInfoTab';
